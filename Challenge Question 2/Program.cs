d1f97f2122e075495d2a4edc2fcf15789aa44a41
ReshaanST <ST10433877@vcconnect.edu.za>
2025-08-11 11:15:27 +0200

    Completed Challenge Question 2

---
+++ Challenge Question 2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Challenge_Question_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Countdown(10);
+        }
+        static void Countdown(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine(number);
+                Countdown(number - 1);
+            }
+            else
+            {
+                Console.WriteLine("Liftoff!");
+            }
+
+        }
+    }
+}
