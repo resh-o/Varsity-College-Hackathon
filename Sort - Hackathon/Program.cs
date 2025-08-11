248c0489a60d52351270b2baf1e29dfe0ba02b23
ReshaanST <ST10433877@vcconnect.edu.za>
2025-08-11 09:30:29 +0200

    Add project files.

---
+++ Sort - Hackathon/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Sort___Hackathon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 5, 2, 9, 1, 5, 6, 3 };
+            Console.WriteLine("Original List: " + numbers);
+
+            SortList(numbers);
+
+            Console.WriteLine("Sorted List in Ascending Order: ");
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+        }
+
+        public static void SortList(List<int> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[i] > list[j])
+                    {
+                        int temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
