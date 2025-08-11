f38366936bd5bea8d93e71644bb7cb36ff69d514
ReshaanST <ST10433877@vcconnect.edu.za>
2025-08-11 10:33:49 +0200

    Completed Challenge Question 0 - Print the number 10 without using any actual numbers.

---
+++ Challenge Quesrtion 0/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Challenge_Quesrtion_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 'n' - 'd'; i <= 'n' - 'd'; i++)
+            {
+                Console.Write(i);
+            }
+        }
+    }
+}
